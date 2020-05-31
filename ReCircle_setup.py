02ba0958ad85b00790e4ab00c2b82f778f01dfc1
ThomasVDD <thomasvandendries@gmail.com>
2020-05-31 13:29:13 +0200

    Updated ssetup script to include can setup

--- ReCircle_setup.py
+++ ReCircle_setup.py
@@ -5,3 +5,7 @@ import sys
 os.system('echo "# ReCircle Startup" | sudo tee -a /etc/crontab')
 os.system('echo "@reboot sudo python3 /usr/ReCircle/ReCircle.py &" | sudo tee -a /etc/crontab')
 
+os.system('echo "# ReCircle Startup" | sudo tee -a /boot/config.txt')
+os.system('echo "dtparam=spi=on" | sudo tee -a /boot/config.txt')
+os.system('echo "dtoverlay=mcp2515-can0,oscillator=16000000,interrupt=25,spimaxfrequency=500000" | sudo tee -a /boot/config.txt')
+os.system('echo "dtoverlay=spi-bcm2835-overlay" | sudo tee -a /boot/config.txt') 
